6c095427c423750d947d0ca8a35c9b11be693c33
farhan <farhanasfar21@gmail.com>
2025-01-19 17:19:28 +0600

    button css

---
+++ chef-claude/src/components/Main.jsx
@@ -0,0 +1,19 @@
+
+
+function Main(){
+    return(
+        <>
+            <form className="add-ingredient-form">
+                <input 
+                type="text" 
+                aria-label="Add ingredient"
+                placeholder="e.g. oregano"/>
+                <button>Add Ingredient</button>
+            </form>
+        </>
+    )
+
+}
+
+
+export default Main;
