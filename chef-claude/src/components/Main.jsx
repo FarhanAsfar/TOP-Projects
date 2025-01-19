8111f2cef147949b12fac3e43461909c0bffe7d1
farhan <farhanasfar21@gmail.com>
2025-01-19 19:34:00 +0600

    form

--- chef-claude/src/components/Main.jsx
+++ chef-claude/src/components/Main.jsx
@@ -1,15 +1,33 @@
 
 
 function Main(){
+    const ingredients = ["chick", "oregano", "tomao"];
+
+    const ingredientList = ingredients.map((ingredient, index) => (
+        <li key={index}>{ingredient}</li>
+    ))
+
+    function handleSubmit(event){
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const newIngredient = formData.get("ingredient");
+        ingredients.push(newIngredient);
+        console.log(ingredients);
+    }
+    
     return(
         <>
-            <form className="add-ingredient-form">
+            <form onSubmit={handleSubmit} className="add-ingredient-form">
                 <input 
+                name="ingredient"
                 type="text" 
                 aria-label="Add ingredient"
                 placeholder="e.g. oregano"/>
                 <button>Add Ingredient</button>
             </form>
+            <ul>
+                {ingredientList}
+            </ul>
         </>
     )
 
