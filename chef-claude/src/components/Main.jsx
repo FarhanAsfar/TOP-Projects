54a56df70c5723bfb3a04cee510917a2008654a7
arnob <farhanasfar21@gmail.com>
2025-01-21 17:59:35 +0600

    conditional rendering to show recipe button

--- chef-claude/src/components/Main.jsx
+++ chef-claude/src/components/Main.jsx
@@ -29,13 +29,13 @@ function Main(){
             {ingredients.length > 0 && <section>
                 <h2>Ingredients on hand: </h2>
                 <ul className="ingredients-list" aria-live="polite">{ingredientList}</ul>
-                <div className="get-recipe-container">
+                {ingredients.length >4 && <div className="get-recipe-container">
                     <div>
                         <h3>Ready for a recipe?</h3>
                         <p>Generate a recipe from your ingredients list.</p>
                     </div>
                     <button>Get a recipe</button>
-                </div>
+                </div>}
             </section>}
         </>
     )
