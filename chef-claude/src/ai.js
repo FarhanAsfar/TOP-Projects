5b6f151cc32f93c599d8e77c7b92cc2497fa7b1a
arnob <farhanasfar21@gmail.com>
2025-01-23 02:39:42 +0600

    new a_token

--- chef-claude/src/ai.js
+++ chef-claude/src/ai.js
@@ -5,11 +5,9 @@ const SYSTEM_PROMPT = `
 You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page.
 `;
 
-<<<<<<< HEAD
 const hf = new HfInference("import.meta.env.REACT_APP_HF_ACCESS_TOKEN");
-=======
-const hf = new HfInference(import.meta.env.REACT_APP_HF_ACCESS_TOKEN);
->>>>>>> 973b456 (complete chef claude)
+
+
 
 export async function getRecipeFromMistral(ingredientsArr) {
     const ingredientsString = ingredientsArr.join(", ");
