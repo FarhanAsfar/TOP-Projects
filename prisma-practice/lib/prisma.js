8b37581fb5acbedce7a89f5b01eb6586120cd3ca
arnob <farhanasfar21@gmail.com>
2025-12-29 23:56:49 +0600

    Update prisma connection file

--- prisma-practice/lib/prisma.js
+++ prisma-practice/lib/prisma.js
@@ -1,10 +1,13 @@
-import "dotenv/config";
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '../generated/prisma/client'
+import pg from "pg";
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import dotenv from "dotenv";
 
-const connectionString = `${process.env.DATABASE_URL}`
+dotenv.config();
 
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
+const connectionString = process.env.DATABASE_URL;
+const pool = new pg.Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
-export { prisma }
+export { prisma };
