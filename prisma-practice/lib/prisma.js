a0103cf03faa52c458c8df911e54b6a59a8e647e
arnob <farhanasfar21@gmail.com>
2025-12-30 03:32:05 +0600

    Fix prisma database connection

--- prisma-practice/lib/prisma.js
+++ prisma-practice/lib/prisma.js
@@ -1,13 +1,10 @@
-import pg from "pg";
-import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import dotenv from "dotenv";
+import "dotenv/config";
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '@prisma/client'
 
-dotenv.config();
+const connectionString = `${process.env.DATABASE_URL}`
 
-const connectionString = process.env.DATABASE_URL;
-const pool = new pg.Pool({ connectionString });
-const adapter = new PrismaPg(pool);
-const prisma = new PrismaClient({ adapter });
+const adapter = new PrismaPg({ connectionString })
+const prisma = new PrismaClient({ adapter })
 
-export { prisma };
+export { prisma }
